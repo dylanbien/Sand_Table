cb3762af0944871866d63745fe488fc5862f73e9
dylanbien <42746139+dylanbien@users.noreply.github.com>
2019-02-19 15:35:32 -0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,7 +1,4 @@
-import numpy as nm
-period = 100
-degrees_per_second = period / 360.0
-
+import numpy as np
 
 def cart2pol(x, y):
     rho = np.sqrt(x**2 + y**2)
@@ -13,32 +10,43 @@ def pol2cart(rho, phi):
     y = rho * np.sin(phi)
     return(x, y)
 
-def move_in_straight_line (self, sides):
-
-    angle_change = 360 / sides
-    increments = 5.0
-    number_of_increments = angle_change / increments
+def make_shape(sides, outward):
     
-    global degrees_per_second
-    time_per_increment = degrees_per_second * increment
+    angle_change = 360 / sides #used in move in straight line call
+    increments = 5.0 #angle change between each x,y point
+    number_of_increments = angle_change / increments #the number of x,y points used the move in straight line
+    
+    if outward:
+        r_change = 20000
+    else:
+        r_change = -20000
 
-    starting_polar = [5, 0]
-    ending_polar = [ starting_polar[0], starting_polar[1] + angle_change ]
+    for count in range (sides):
+        move_in_straight_line(motor.get_pos(), i * angle_change, r_change)
+    
+    
+def move_in_straight_line (self, starting_r, starting_theta, r_change): 
+    global increments 
+    
+    #gets the starting and ending polar cordinates
+    starting_polar = [starting_r, starting_theta]
+    ending_polar = [starting_polar[0] + r_change, starting_polar[1] + angle_change ]
 
+    #transfers the above to cartesian
     starting_x, starting_y = pol2cart(starting_polar[0], starting_polar[1])
     ending_x, ending_y = pol2cart(ending_polar[0], ending_polar[1])
 
     #use these two points to make a line: y = mx + b
-    
     slope = (ending_y - starting_y) / (ending_x - starting_x) #m
     y_intercept = ending_y - (slope * ending_x) #b
     
+    
     angles = []
     radii = []
-    for i in range(starting_polar[1], ending_polar[1] + increments , increments):
+    for ang in range(starting_polar[1], ending_polar[1] + increments , increments):
         
-        angles.append(i)
-        radii.append( y_intercept / (sin(i) - (slope * cos (i) ) ) )
+        angles.append(ang)
+        radii.append( y_intercept / (sin(ang) - (slope * cos (ang) ) ) ) #note: may need to change angles from theta to radians?
         
     print(angles)
     print(radii)
@@ -47,17 +55,16 @@ def move_in_straight_line (self, sides):
     a = 0
     b = 1
     
-    for l in range(number_of_increments):
+    for i in range(number_of_increments):
         radius_change.append(radii[b] - radii[a])
         a += 1
         b += 1
-        
     print (radius_change)
-  
+ 
     velocities = []
     
-    for j in radius_change:
-        velocities.append( j / time_per_increment )
+    for distance in radius_change:
+        velocities.append( distance / time_per_increment ). #work on time increment
         
         
     #for l in range (number_of_increments):
