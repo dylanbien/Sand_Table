e4c853a585eb9c3a04028e80ab85c13bca86515e
ngetachew <natugetachew@gmail.com>
2019-02-20 12:54:02 -0800

    Worked on creating straight line graphs

--- testing.py
+++ testing.py
@@ -1,32 +1,40 @@
 import numpy as np
 import graph as g
 
+angle_change = None
+increments = 5
+number_of_increments = None
+radius_time = 1
+radius_speed_velocity_relationship = 1
+
 def cart2pol(x, y):
     rho = np.sqrt(x**2 + y**2)
     phi = np.arctan2(y, x)
-    print(rho)
-    print(phi)
-    return(rho, phi)
+    #print(rho)
+    #print(phi)
+    return(int(rho), int(phi))
 
 def pol2cart(rho, phi):
     x = rho * np.cos(np.deg2rad(phi))
     y = rho * np.sin(np.deg2rad(phi))
-    print(x)
-    print(y)
-    return(x, y)
+    #print(x)
+    #print(y)
+    return(int(x), int(y))
 
 def make_shape(sides, outward):
-    
+    global angle_change
+    global increments
+    global number_of_increments
     #theta information
     period = 32 #seconds
     steps_per_period = 300000 
     theta_speed = steps_per_period / period
-    theta_speed_velocity_relationship = theta_speed / theta_motor.get_vel()
+    #theta_speed_velocity_relationship = theta_speed / theta_motor.get_vel()
     
    
     #angle informaton
     angle_change = 360 / sides #used in move in straight line call
-    increments = 5.0 #angle change between each x,y point
+
     number_of_increments = angle_change / increments #the number of x,y points used the move in straight line
     
     
@@ -38,16 +46,19 @@ def make_shape(sides, outward):
     else:
         r_change = 0
 
-    for count in range (0, sides):
-        move_in_straight_line(motor.get_pos(), i * angle_change, r_change)
+    #for count in range (0, sides):
+    move_in_straight_line(200,0, r_change)
     
     
-def move_in_straight_line (self, starting_r, starting_theta, r_change): 
+def move_in_straight_line(starting_r, starting_theta, r_change):
     global increments 
-    
+    global angle_change
+    global number_of_increments
+    global radius_time
+    global radius_speed_velocity_relationship
     #gets the starting and ending polar cordinates
-    starting_polar = [starting_r, starting_theta]
-    ending_polar = [starting_polar[0] + r_change, starting_polar[1] + angle_change ]
+    starting_polar = [starting_r, int(starting_theta)]
+    ending_polar = [starting_polar[0] + r_change, int(starting_polar[1] + angle_change) ]
 
     #transfers the above to cartesian
     starting_x, starting_y = pol2cart(starting_polar[0], starting_polar[1])
@@ -63,22 +74,21 @@ def move_in_straight_line (self, starting_r, starting_theta, r_change):
     for ang in range(starting_polar[1], ending_polar[1] + increments , increments):
         
         angles.append(ang)
-        radii.append( y_intercept / (np.sin(np.deg2rad(ang)) - (slope * np.cos(np.deg2rad(ang)) ) ) ) #note: may need to change angles from theta to radians?
+        radii.append(int( y_intercept / (np.sin(np.deg2rad(ang)) - (slope * np.cos(np.deg2rad(ang)) ) ) )) #note: may need to change angles from theta to radians?
         
     print(angles)
     print(radii)
-
-    g.plotgraph(radii, np.rad2deg(angles))
+    g.plotgraph(radii, angles)
 
     radius_change = []    
     a = 0
     b = 1
     
-    for i in range(number_of_increments):
+    for i in range(int(number_of_increments)):
         radius_change.append(radii[b] - radii[a])
         a += 1
         b += 1
-    print (radius_change)
+    print(radius_change)
 
 
     velocities = []
@@ -86,4 +96,7 @@ def move_in_straight_line (self, starting_r, starting_theta, r_change):
     for distance in radius_change:
         speed = distance / radius_time #steps per second
         velocities.append(speed * radius_speed_velocity_relationship)
-        
+
+
+
+make_shape(3, True)
