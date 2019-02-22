2bfcd1ade97a5c4095a6da52d283fcc37156de05
ngetachew <natugetachew@gmail.com>
2019-02-22 09:21:09 -0800

    Graph simulation fully working

--- testing.py
+++ testing.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 angle_change = None
 increments = 5
@@ -17,14 +18,14 @@ def cart2pol(x, y):
     phi = np.arctan2(y, x)
     #print(rho)
     #print(phi)
-    return(int(rho), int(phi))
+    return(rho, phi)
 
 def pol2cart(rho, phi):
     x = rho * np.cos(np.deg2rad(phi))
     y = rho * np.sin(np.deg2rad(phi))
     #print(x)
     #print(y)
-    return(int(x), int(y))
+    return(x, y)
 
 def make_shape(sides, outward):
     global angle_change
@@ -39,6 +40,32 @@ def make_shape(sides, outward):
     
     
     #radius information
+    radius_time = (period / 360) * increments
+    if outward:
+        r_change = -2
+    else:
+        r_change = -11
+
+    start = 200
+    for i in range(1):
+        for count in range(0, sides):
+            start = start + r_change
+            if (start < 0):
+                print('start below zero')
+                return
+            move_in_straight_line(start, int(count * angle_change), r_change)
+
+
+def make_shape_spiral(sides, outward):
+    global angle_change
+    global increments
+    global number_of_increments
+
+    # angle informaton
+    angle_change = 360 / sides  # used in move in straight line call
+    number_of_increments = angle_change / increments  # the number of x,y points used the move in straight line
+
+    # radius information
     radius_time = (period / 360) * increments
     if outward:
         r_change = -5
@@ -62,35 +89,37 @@ def move_in_straight_line(starting_r, starting_theta, r_change):
     global radius_time
     global radius_speed_velocity_relationship
     #gets the starting and ending polar cordinates
-    starting_polar = [starting_r, int(starting_theta)]
-    ending_polar = [starting_polar[0] + r_change, int(starting_polar[1] + angle_change) ]
+    starting_polar = [starting_r, starting_theta]
+    ending_polar = [starting_polar[0] + r_change, starting_polar[1] + angle_change]
 
     #transfers the above to cartesian
     starting_x, starting_y = pol2cart(starting_polar[0], starting_polar[1])
     ending_x, ending_y = pol2cart(ending_polar[0], ending_polar[1])
 
     #use these two points to make a line: y = mx + b
-    slope = (ending_y - starting_y) / (ending_x - starting_x) #m
+    slope = (ending_y - starting_y) / (ending_x - starting_x)
     y_intercept = ending_y - (slope * ending_x) #b
     
     
     angles = []
     radii = []
-    for ang in range(starting_polar[1], ending_polar[1] + increments , increments):
+
+
+
+    for ang in range(int(starting_polar[1]), int(ending_polar[1]+increments)  , increments):
         
         angles.append(ang)
-        radii.append(int( y_intercept / (np.sin(np.deg2rad(ang)) - (slope * np.cos(np.deg2rad(ang)) ) ) )) #note: may need to change angles from theta to radians?
+        radii.append(y_intercept / (np.sin(np.deg2rad(ang)) - (slope * np.cos(np.deg2rad(ang)) ) ) ) #note: may need to change angles from theta to radians?
         plt.polar(angles[len(angles)-1]*(np.pi/180), radii[len(angles)-1],'k.')
 
     print(angles)
     print(radii)
 
-
     radius_change = []    
     a = 0
     b = 1
     
-    for i in range(int(number_of_increments)):
+    for i in range(int(number_of_increments )):
         radius_change.append(radii[b] - radii[a])
         a += 1
         b += 1
@@ -111,7 +140,7 @@ def move_in_straight_line(starting_r, starting_theta, r_change):
         continue
 
 ax = plt.subplot(111, projection='polar')
-make_shape(5, True)
+make_shape_spiral(6, True)
 ax.set_rmax(210)
 ax.set_rticks([0.5, 1, 1.5, 2])  # less radial ticks
 ax.set_rlabel_position(-22.5)  # get radial labels away from plotted line
