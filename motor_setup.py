5862eb738e0c591c9d26f8b6aa9ee4a0e06ace73
Alex Oaten <alex.oaten@gmail.com>
2019-03-01 08:13:42 -0800

    edited motor set up

--- motor_setup.py
+++ motor_setup.py
@@ -5,18 +5,10 @@ radius = odrive.find_any()
 
 print("found odrive: " + str(radius.serial_number))
 
-motor1 = ODrive_Ease_Lib.ODrive_Axis(radius.axis0)
+motor = ODrive_Ease_Lib.ODrive_Axis(radius.axis0)
 #o_motor = ODrive_Ease_Lib.ODrive_Axis(radius.axis1)
 
-motor1.calibrate()
-#o_motor.calibrate()
+motor.calibrate()
 
-#o_motor.set_curr_limit(15)
-motor1.set_vel_gain(.0001)
-motor1.get_vel_gain()
-
-#o_motor.set_vel(20000)
-
-
-#o_motor.home_with_vel(20000)
+motor.home_with_vel(20000)
 
